a19109c5d0280a232248f8dca0be3485ead6be52
Orsi <punkah@gmail.com>
2018-05-20 09:35:18 +1000

    new branch with simple react

---
+++ quickshift/simple/src/App.js
@@ -0,0 +1,102 @@
+import "bulma/css/bulma.css";
+import React, { Component } from "react";
+import "./App.css";
+import logo from "./logo.svg";
+import sms from "./sms";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      yes: false
+    };
+  }
+
+  componentDidMount() {
+    sms.sendsms().then(message => {
+      console.log(message);
+      this.setState({ yes: true });
+    });
+  }
+
+  render() {
+    const { yes } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <p className="App-intro">{yes && <div>Heather answered yes!</div>}</p>
+
+        <section className="hero is-info is-large">
+          <div className="hero-head">
+            <nav className="navbar">
+              <div className="container">
+                <div className="navbar-brand">
+                  <span
+                    className="navbar-burger burger"
+                    data-target="navbarMenuHeroB"
+                  >
+                    <span />
+                    <span />
+                    <span />
+                  </span>
+                </div>
+                <div id="navbarMenuHeroB" className="navbar-menu">
+                  <div className="navbar-end">
+                    <a className="navbar-item is-active">Home</a>
+                    <a className="navbar-item">Examples</a>
+                    <a className="navbar-item">Documentation</a>
+                    <span className="navbar-item">
+                      <a className="button is-info is-inverted">
+                        <span className="icon">
+                          <i className="fab fa-github" />
+                        </span>
+                        <span>Download</span>
+                      </a>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </nav>
+          </div>
+
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <p className="title">QuickShift</p>
+              <p className="subtitle">Last minute shift rescheduling</p>
+            </div>
+          </div>
+          <div className="hero-foot">
+            <nav className="tabs is-boxed is-fullwidth">
+              <div className="container">
+                <ul>
+                  <li className="is-active">
+                    <a>Overview</a>
+                  </li>
+                  <li>
+                    <a>Modifiers</a>
+                  </li>
+                  <li>
+                    <a>Grid</a>
+                  </li>
+                  <li>
+                    <a>Elements</a>
+                  </li>
+                  <li>
+                    <a>Components</a>
+                  </li>
+                  <li>
+                    <a>Layout</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ quickshift/simple/src/sms.js
@@ -0,0 +1,57 @@
+import request from "request-promise-native";
+
+const sendsms = async () => {
+  const response = await request({
+    uri: "https://tapi.telstra.com/v2/oauth/token",
+    form: {
+      client_id: "LjX80rZK5EUBAKWC1MgOxhuTfp0cxqRm",
+      client_secret: "oYQtTr446AmO9I7k",
+      grant_type: "client_credentials"
+    },
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    method: "post"
+  });
+  const jsonResponse = JSON.parse(response);
+  console.log(jsonResponse.access_token);
+
+  await request({
+    uri: "https://tapi.telstra.com/v2/messages/sms",
+    body: {
+      to: "+61478124225",
+      validity: "60",
+      priority: false,
+      body: "hey from telstra",
+      replyRequest: true
+    },
+    method: "post",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jsonResponse.access_token}`
+    },
+    json: true
+  });
+
+  while (true) {
+    console.log("waiting...");
+    setTimeout(() => {}, 1000);
+    const answer = await request({
+      uri: `https://tapi.telstra.com/v2/messages/sms`,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jsonResponse.access_token}`
+      },
+      method: "get"
+    });
+    if (answer.message) {
+      return answer.message;
+    }
+  }
+};
+
+export default {
+  sendsms
+};
